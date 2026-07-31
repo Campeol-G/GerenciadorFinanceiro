c40c504223c82e43289ea68f99460b04d8f977e5
ChicoMagnusOpus <gustavocampeol28@gmail.com>
2026-07-30 23:35:42 -0300

    Main View controller

---
+++ src/main/java/com/AppProject/GFinanciero/controller/MainViewController.java
@@ -0,0 +1,42 @@
+package com.AppProject.GFinanciero.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import org.springframework.stereotype.Controller;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.MenuItem;
+
+@Controller
+public class MainViewController implements Initializable {
+
+  @FXML
+  private MenuItem menuItemSeller;
+  @FXML
+  private MenuItem menuItemDepartment;
+  @FXML
+  private MenuItem menuItemAbout;
+
+  @FXML
+  public void onMenuItemSellerAction() {
+    System.out.println("onMenuItemSellerAction");
+  }
+
+  @FXML
+  public void onMenuItemDepartmentAction() {
+    System.out.println("onMenuItemDepartmentAction");
+  }
+
+  @FXML
+  public void onMenuItemAboutAction() {
+    System.out.println("onMenuItemAboutAction");
+  }
+
+  @Override
+  public void initialize(URL arg0, ResourceBundle arg1) {
+
+  }
+
+}
